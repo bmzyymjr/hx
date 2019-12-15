3e68a1271ed5580954120739199fef1d95c5cc89
何欣 <3077637663@qq.com>
2019-12-15 10:53:15 +0800

    何欣的优化上传

--- bignews/static/js/hexin/list.js
+++ bignews/static/js/hexin/list.js
@@ -1,52 +1,52 @@
 var nav = 1
 var articleId = 1
-    // 获取文章所有类别
+// 获取文章所有类别
 $.ajax({
-        type: 'get',
-        url: 'http://localhost:8080/api/v1/admin/category/list',
-        success: function(response) {
-            var html = template('categorylistTpl', { data: response.data })
-            $("#categorylist").html(html)
-            $('#categories').html(html)
-        }
-    })
-    // 获取文章热门排行
+    type: 'get',
+    url: 'http://localhost:8080/api/v1/admin/category/list',
+    success: function (response) {
+        var html = template('categorylistTpl', { data: response.data })
+        $("#categorylist").html(html)
+        $('#categories').html(html)
+    }
+})
+// 获取文章热门排行
 $.ajax({
-        type: 'get',
-        url: 'http://localhost:8080/api/v1/index/rank',
-        success: function(response) {
-            // console.log(response)
-            response.data.forEach((item, index) => item.index = index)
-            var html = template('hotlistTpl', { data: response.data })
-            $('#hotlist').html(html)
-        }
-    })
-    // 获取最新评论数据并展示
+    type: 'get',
+    url: 'http://localhost:8080/api/v1/index/rank',
+    success: function (response) {
+        // console.log(response)
+        response.data.forEach((item, index) => item.index = index)
+        var html = template('hotlistTpl', { data: response.data })
+        $('#hotlist').html(html)
+    }
+})
+// 获取最新评论数据并展示
 $.ajax({
-        type: 'get',
-        url: 'http://localhost:8080/api/v1/index/latest_comment',
-        success: function(response) {
-            var date1 = +new Date()
-            response.data.forEach(item => {
-                var date2 = +new Date(item.date)
-                var lastTime = Math.floor((date1 - date2) / 1000 / 60 / 60 / 24 / 30)
-                item.time = lastTime
-            })
-            var html = template('lastedcommentTpl', { data: response.data })
-            $('#lastedcomment').html(html)
-        }
-    })
-    // 获得焦点关注的数据并展示
+    type: 'get',
+    url: 'http://localhost:8080/api/v1/index/latest_comment',
+    success: function (response) {
+        var date1 = +new Date()
+        response.data.forEach(item => {
+            var date2 = +new Date(item.date)
+            var lastTime = Math.floor((date1 - date2) / 1000 / 60 / 60 / 24 / 30)
+            item.time = lastTime
+        })
+        var html = template('lastedcommentTpl', { data: response.data })
+        $('#lastedcomment').html(html)
+    }
+})
+// 获得焦点关注的数据并展示
 $.ajax({
-        type: 'get',
-        url: 'http://localhost:8080/api/v1/index/attention',
-        success: function(response) {
-            // console.log(response)
-            var html = template('hotfocusTpl', { data: response.data })
-            $('#hotfocus').html(html)
-        }
-    })
-    // 获取分类列表内容并展示在页面上
+    type: 'get',
+    url: 'http://localhost:8080/api/v1/index/attention',
+    success: function (response) {
+        // console.log(response)
+        var html = template('hotfocusTpl', { data: response.data })
+        $('#hotfocus').html(html)
+    }
+})
+// 获取分类列表内容并展示在页面上
 if (location.search) {
     var par = location.search.substr(1).split('=')[0]
     var params = location.search.substr(1).split('=')[1]
@@ -55,7 +55,7 @@ if (location.search) {
             type: 'get',
             url: 'http://localhost:8080/api/v1/index/search',
             data: { type: params },
-            success: function(response) {
+            success: function (response) {
                 nav = Math.ceil(response.data.totalCount / 6)
                 var html = template('articlelistTpl', { data: response.data.data })
                 $('#articlelist').html(html)
@@ -67,9 +67,9 @@ if (location.search) {
             type: 'get',
             url: 'http://localhost:8080/api/v1/index/search',
             data: { key: params },
-            success: function(response) {
+            success: function (response) {
                 nav = Math.ceil(response.data.totalCount / 6)
-                    // response.data.data[0].category = '搜索结果'
+                // response.data.data[0].category = '搜索结果'
                 var html = template('articlelistTpl', { data: response.data.data })
                 $('#articlelist').html(html)
             }
@@ -80,7 +80,7 @@ if (location.search) {
         type: 'get',
         url: 'http://localhost:8080/api/v1/index/search',
         data: { type: 1 },
-        success: function(response) {
+        success: function (response) {
             nav = Math.ceil(response.data.totalCount / 6)
             var html = template('articlelistTpl', { data: response.data.data })
             $('#articlelist').html(html)
@@ -89,36 +89,38 @@ if (location.search) {
 }
 
 // 给搜索按钮添加点击事件
-$('#searchbtn').on('click', function() {
+$('#searchbtn').on('click', function () {
     location.href = 'http://localhost:8080/list.html?key=' + $(this).siblings().val()
 })
 
 // 给全部分类注册点击事件，获取分类id
-$('#categories').on('click', 'a', function() {
+$('#categories').on('click', 'a', function () {
     var id = $(this).attr('data-id')
     articleId = id
     categoryId(id)
+    location.href = 'http://localhost:8080/list.html?id=' + id
     return false
 })
-$('#categorylist').on('click', 'a', function() {
+$('#categorylist').on('click', 'a', function () {
     var id = $(this).attr('data-id')
     articleId = id
     categoryId(id)
+    location.href = 'http://localhost:8080/list.html?id=' + id
     return false
 })
-$(function() {
-    setTimeout(function() {
+$(function () {
+    setTimeout(function () {
 
         $("#pagination").pagination({
             currentPage: 1,
             totalPage: nav,
-            callback: function(current) {
+            callback: function (current) {
                 // alert('ok!');
             }
         });
         var text = 1
-            // 为分页数添加点击事件
-        $('#pagination').on('click', 'a', function() {
+        // 为分页数添加点击事件
+        $('#pagination').on('click', 'a', function () {
             var current = $(this).text()
             if (current == '上一页') {
                 text = text - 1
@@ -146,7 +148,7 @@ function run() {
     $("#pagination").pagination({
         currentPage: 1,
         totalPage: nav,
-        callback: function(current) {
+        callback: function (current) {
             // alert('ok!');
         }
     });
@@ -159,7 +161,7 @@ function categoryId(ids, index) {
         type: 'get',
         url: 'http://localhost:8080/api/v1/index/search',
         data: { type: ids, page: index },
-        success: function(response) {
+        success: function (response) {
             nav = Math.ceil(response.data.totalCount / 6)
             var html = template('articlelistTpl', { data: response.data.data })
             $('#articlelist').html(html)
